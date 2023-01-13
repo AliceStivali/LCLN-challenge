f3b8f1b96e8775d3f0aa256d04773ba5ab0bb401
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2023-01-13 16:56:32 +0100

    boats shapes added with hover animation

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,17 @@ export function Home() {
         <div className="title">Lorem ipsum</div>
       </div>
       <div className="aside"></div>
-      <div className="main"></div>
+      <div className="main">
+        <div className="first-line">
+          <div className="boat red-boat"></div>
+          <div className="boat yellow-boat"></div>
+          <div className="boat green-boat"></div>
+        </div>
+        <div className="second-line">
+          <div className="boat green-boat"></div>
+          <div className="boat red-boat"></div>
+        </div>
+      </div>
     </div>
   );
 }

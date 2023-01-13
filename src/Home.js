2e931f22ff03448310f1ae9a134025925adc164c
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2023-01-13 16:24:50 +0100

    main spaces defined

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,11 @@
 export function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="title">Lorem ipsum</div>
+      </div>
+      <div className="aside"></div>
+      <div className="main"></div>
+    </div>
+  );
 }
